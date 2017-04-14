16e152828d37b2ce28fa2294562b24147c39f232
aboveyou00 <brandonyoyoslade@gmail.com>
2017-04-13 21:24:21 -0600

    feat(launch-config): add more default configuration values

--- config/default.js
+++ config/default.js
@@ -1,9 +1,30 @@
-
+//Important: overwrite important config values in a local-development.js file, not here!
 
 module.exports = {
     logger: {
         logLevel: {
             default: 'info'
         }
-    }
+    },
+    
+    connections: {
+        db: {
+            name:     process.env.DB_NAME,
+            user:     process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            host: {
+                domain: process.env.DB_HOST,
+                port:   process.env.DB_PORT
+            },
+            dialect: 'mysql',
+            charset: 'utf8',
+            
+            pool: {
+                max: 1000,
+                min: 5,
+                idle: 5 * 60 * 1000 //Five minutes
+            }
+        }
+    },
+    
 }
--- src/server/launch-config.ts
+++ src/server/launch-config.ts
@@ -1,18 +1,31 @@
-import { Miter, ServerMetadataT } from 'miter';
+import { Miter, ServerMetadataT, DatabaseMetadataT, Charset } from 'miter';
 import { config } from './util/config';
+import { AppModels } from './models';
+import { AppServices } from './services';
+import { AppControllers } from './controllers';
 
 export const DEFAULT_SERVER_PORT = 8081;
+
+let ormConfig = config.try<DatabaseMetadataT>('connections.db');
 
 export const commonConfig: ServerMetadataT = {
     name: config.try<string>('server.name', 'brandonslade.me'),
     
-    port: Miter.normalizePort(config.try<any>('server.port', DEFAULT_SERVER_PORT)),
-    router: {
-        path: 'api'
+    orm: {
+        enabled: !!ormConfig,
+        models: AppModels,
+        
+        db: ormConfig
     },
     
-    orm: {
-        enabled: false
+    services: AppServices,
+    
+    port: Miter.normalizePort(config.try<any>('server.port', DEFAULT_SERVER_PORT)),
+    router: {
+        path: 'api',
+        controllers: AppControllers,
+        middleware: [],
+        policies: []
     },
     
     logLevel: config.try<any>('logger.logLevel')
