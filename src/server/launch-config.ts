183ea2747da83220d1e52e08e3814fd458c0166c
aboveyou00 <brandonyoyoslade@gmail.com>
2017-04-13 20:49:39 -0600

    build(npm): add miter and miter-cli; add simple launch-config

---
+++ src/server/launch-config.ts
@@ -0,0 +1,8 @@
+import { ServerMetadataT } from 'miter';
+
+export const DEFAULT_SERVER_PORT = 8081;
+
+export const commonConfig: ServerMetadataT = {
+    name: 'brandonslade.me',
+    port: DEFAULT_SERVER_PORT
+};
--- src/tasks/server.ts
+++ src/tasks/server.ts
@@ -1,5 +1,7 @@
 import { Task } from 'ts-task';
+import { Miter } from 'miter';
 
 export const ServerTask: Task = new Task('server', (...args: any[]) => {
-    console.log('In server start task!');
+    let launchConfig = require('../server/launch-config');
+    Miter.launch(launchConfig);
 });
